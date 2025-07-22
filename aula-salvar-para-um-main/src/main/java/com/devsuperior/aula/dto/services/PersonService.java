ccfdae530e251df64da45f007e30b7e93217fce5
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-07-22 20:56:41 -0300

    first commit

---
+++ aula-salvar-para-um-main/src/main/java/com/devsuperior/aula/dto/services/PersonService.java
@@ -0,0 +1,17 @@
+package com.devsuperior.aula.dto.services;
+
+import com.devsuperior.aula.dto.DepartmentDTO;
+import com.devsuperior.aula.entities.Person;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    public DepartmentDTO insert(DepartmentDTO dto){
+
+        Person entity= new Person();
+    }
+
+
+
+}
