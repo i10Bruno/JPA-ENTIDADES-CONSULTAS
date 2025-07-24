8920c1d9482978abed0ef30d7ce0bcac462627f5
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-07-24 20:44:51 -0300

    entendendo relações banco

--- aula-salvar-para-um-main/src/main/java/com/devsuperior/aula/dto/DepartmentDTO.java
+++ aula-salvar-para-um-main/src/main/java/com/devsuperior/aula/dto/DepartmentDTO.java
@@ -1,5 +1,7 @@
 package com.devsuperior.aula.dto;
 
+import com.devsuperior.aula.entities.Department;
+
 public class DepartmentDTO {
 
     private Long id;
@@ -8,6 +10,12 @@ public class DepartmentDTO {
     public DepartmentDTO(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    // tentar entender essa parte depois
+    public DepartmentDTO(Department entity) {
+        id = entity.getId();
+        name = entity.getName();
     }
 
 
--- aula-salvar-para-um-main/src/main/java/com/devsuperior/aula/services/PersonService.java
+++ aula-salvar-para-um-main/src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -1,15 +1,30 @@
 package com.devsuperior.aula.services;
 
 import com.devsuperior.aula.dto.DepartmentDTO;
+import com.devsuperior.aula.dto.PersonDepartmentDTO;
+import com.devsuperior.aula.entities.Department;
 import com.devsuperior.aula.entities.Person;
+import com.devsuperior.aula.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 
-    public DepartmentDTO insert(DepartmentDTO dto){
+    @Autowired
+    private PersonRepository repository;
+
+    public PersonDepartmentDTO insert (PersonDepartmentDTO dto){
 
         Person entity= new Person();
+        entity.setName(dto.getName());
+        entity.setSalary(dto.getSalary());
+        Department dept = new Department();
+        dept.setId(dto.getDepartment().getId());
+        entity.setDepartment(dept);
+        entity=repository.save(entity);
+
+        return new PersonDepartmentDTO(entity);
     }
 
 
